f88875692d73cb2a3816a9dd3866e0c6ef4bbcd8
Josh Nussbaum <joshnuss@gmail.com>
2014-04-28 23:24:48 -0400

    Added Summary#orders

--- lib/spree_recap/summary.rb
+++ lib/spree_recap/summary.rb
@@ -9,5 +9,9 @@ module SpreeRecap
     def registrations
       @registrations ||= User.where(created_at: @range)
     end
+
+    def orders
+      @orders ||= Order.complete.order('completed_at desc').where(completed_at: @range)
+    end
   end
 end
--- spec/lib/summary_spec.rb
+++ spec/lib/summary_spec.rb
@@ -6,8 +6,16 @@ describe SpreeRecap::Summary do
   context "registrations" do
     let!(:user1) { create(:user, created_at: 1.hour.ago) }
     let!(:user2) { create(:user, created_at: 2.hour.ago) }
-    let!(:user3) { create(:user, created_at: 2.days.ago) }
+    let!(:old_user) { create(:user, created_at: 2.days.ago) }
 
     specify { summary.registrations.should == [user1, user2] }
+  end
+
+  context "orders" do
+    let!(:order1) { create(:order, completed_at: 1.hour.ago) }
+    let!(:order2) { create(:order, completed_at: 2.hour.ago) }
+    let!(:old_order) { create(:order, completed_at: 2.days.ago) }
+
+    specify { summary.orders.should == [order1, order2] }
   end
 end
