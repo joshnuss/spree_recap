0114b1fddab1ca965154b0a45c667a307ef487a6
Josh Nussbaum <joshnuss@gmail.com>
2014-04-28 22:49:29 -0400

    Refactor Summary#registrations specs

--- spec/lib/summary_spec.rb
+++ spec/lib/summary_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe SpreeRecap::Summary do
-  let!(:user1) { create(:user) }
-  let!(:user2) { create(:user) }
-  let!(:user3) { create(:user, created_at: 2.days.ago) }
-
   let(:summary) { SpreeRecap::Summary.new(1.day.ago..Time.now) }
 
-  specify { summary.registrations.should == [user1, user2] }
+  context "registrations" do
+    let!(:user1) { create(:user, created_at: 1.hour.ago) }
+    let!(:user2) { create(:user, created_at: 2.hour.ago) }
+    let!(:user3) { create(:user, created_at: 2.days.ago) }
+
+    specify { summary.registrations.should == [user1, user2] }
+  end
 end
