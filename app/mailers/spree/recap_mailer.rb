1bd56f465f68d54eee2e13cb52a0c8871e64da60
Josh Nussbaum <joshnuss@gmail.com>
2014-04-30 20:45:46 -0400

    Added subject for summary mailer

--- app/mailers/spree/recap_mailer.rb
+++ app/mailers/spree/recap_mailer.rb
@@ -3,8 +3,21 @@ module Spree
     def summary(range)
       @summary = SpreeRecap::Summary.new(range)
       to_address = @summary.admin_users.map(&:email)
+      subject = "[#{Spree::Config.site_name}] "
 
-      mail(from: from_address, bcc: to_address)
+      if single_day?(range)
+        subject += t(:recap_single_day, date: range.begin.to_date)
+      else
+        subject += t(:recap_multiple_days, from: range.begin.to_date, to: range.end.to_date)
+      end
+
+      mail(from: from_address, bcc: to_address, subject: subject)
+    end
+
+  private
+
+    def single_day?(range)
+      (range.end.to_date - range.begin.to_date) <= 1
     end
   end
 end
--- spec/mailers/recap_mailer_spec.rb
+++ spec/mailers/recap_mailer_spec.rb
@@ -5,9 +5,20 @@ describe Spree::RecapMailer do
     let!(:admin_user1) { create(:admin_user, email: 'admin1@example.com') }
     let!(:admin_user2) { create(:admin_user, email: 'admin2@example.com') }
 
-    subject { Spree::RecapMailer.summary(1.day.ago..Time.now) }
+    context "single day" do
+      subject { Spree::RecapMailer.summary(1.day.ago..Time.now) }
 
-    its(:from) { should == ['spree@example.com'] }
-    its(:bcc) { should == ['admin1@example.com', 'admin2@example.com'] }
+      its(:from) { should == ['spree@example.com'] }
+      its(:bcc) { should == ['admin1@example.com', 'admin2@example.com'] }
+      its(:subject) { should == "[Spree Demo Site] Recap for #{1.day.ago.to_date.to_s}"}
+    end
+
+    context "multiple days" do
+      subject { Spree::RecapMailer.summary(3.days.ago..Time.now) }
+
+      its(:from) { should == ['spree@example.com'] }
+      its(:bcc) { should == ['admin1@example.com', 'admin2@example.com'] }
+      its(:subject) { should == "[Spree Demo Site] Recap for #{3.days.ago.to_date.to_s} to #{Date.today.to_s}"}
+    end
   end
 end
